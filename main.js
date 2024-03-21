c3eb4cec129b1c4a6179000470196dae79f5522d
siapri21 <siapriouattara21@gmail.com>
2024-03-22 00:19:42 +0100

    last commit

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-const doListe = document.querySelector(".accordion ul label");
+//selectionner element de la fonction
+// document.addEventListener("DOMContentLoaded",function(){
+//     const listeItems = document.querySelectorAll(".accordion")
+//     listeItems.forEach(function(liste){
+//         liste.addEventListener("click", function(){
+//             const idLabel =liste.querySelector("label").textContent;
 
-// document.addEventListener('click' , ()=>{
-//      document.querySelector('ul').doListe.
+//             console.log("texte de label : " + idLabel);
+//         })
+//     })
 // })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Sélectionne tous les labels dans la liste d'accordéon
+    const labels = document.querySelectorAll(".accordion label");
+  
+    // Pour chaque label, ajoute un écouteur d'événements pour le clic
+    labels.forEach(function(label) {
+      label.addEventListener("dblclick", function() {
+        // Sélectionne le bouton radio précédent au label et inverse son état
+    const radioBtn = this.previousElementSibling;
+        radioBtn.checked = !radioBtn.checked; // Permute l'état du bouton radio
+      });
+    });
+  });
